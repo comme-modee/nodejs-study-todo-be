0b53927b7cc97b786630aa462edf37719b984a86
Modee <144014486+comme-modee@users.noreply.github.com>
2024-08-27 10:23:22 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors'); //cors에러 해결하기 위한 라이브러리. 자바스크립트는 기본적으로 다른 도메인에서 리소스를 가져오는 걸 보안상 제한한다. 그렇기 때문에 특정 도메인이 안전하다고 허가를 해줘야한다.
 const bodyParser = require('body-parser');
-const indexRouter = require('./routes/index');
-require('dotenv').config();
+const cors = require('cors');
+
+const indexRouter = require('./routes/index')
 const app = express();
+const MONGODB_URI = process.env.MONGODB_URI_PROD;
+const PORT = process.env.PORT;
 
-// app.use(cors({
-//     origin: 'https://main--comme-study-todo.netlify.app'
-// }));
+require('dotenv').config();
+
 app.use(cors());
-app.use(bodyParser.json());
-app.use('/api', indexRouter) //api라는 주소로 입장하면 indexRouter를 사용할것이라는 뜻.
-
-const mongoURI = process.env.MONGODB_URI; //몽구스에 만들어질 폴더이름
+app.use(bodyParser.json()) //body-parser를 사용해 req 바디를 쉽게 읽어올 수 있다.
+app.use('/api', indexRouter) // '/api'로 접속하면 indexRouter를 사용할것이다.
 
 mongoose
-    .connect(mongoURI, { useNewUrlParser: true })
-    .then(() => {
-        console.log('mongoose connected')
+    .connect(MONGODB_URI, {useNewUrlParser: true})
+    .then(()=>{
+        console.log('mongoose connected');
     })
     .catch((err) => {
-        console.log('DB connection fail', err)
-    });
+        console.log('DB connection fail', err);
+    })
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log('server on')
+app.listen(PORT || 5000, () => {
+    console.log(`server on ${PORT || 5000}`);
 })
